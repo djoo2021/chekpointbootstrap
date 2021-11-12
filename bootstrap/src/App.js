00ea16ee8bb86ccf8986bc4d2b7757c71a932956
wajdi mejri <wajdi mejri@apal.local>
2021-11-12 14:54:35 +0100

    bootstrapcommit

---
+++ bootstrap/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import FormComponent from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      
+     <FormComponent />
+  
+    </div>
+  );
+}
+
+export default App;
